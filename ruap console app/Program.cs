7402f96ef5c9b82b93d002d39b21c628290c68b8
DominikZ <dzivko@etfos.hr>
2021-11-03 17:03:10 +0100

    fourth

--- ruap console app/Program.cs
+++ ruap console app/Program.cs
@@ -6,7 +6,11 @@ namespace ruap_console_app
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Martine Damjanović 0118656934!");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Martine Damjanović!");
+>>>>>>> e59218e (initial)
         }
     }
 }
